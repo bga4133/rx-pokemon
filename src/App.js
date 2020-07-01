d84f739ac369b6206bcdefb7ea62ab4c279b3f79
Mac <mac@julito-MBP13.local>
2020-07-01 16:18:44 +0200

    firts commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { from, BehaviorSubject } from "rxjs";
+import {
+  filter,
+  mergeMap,
+  debounceTime,
+  distinctUntilChanged
+} from "rxjs/operators";
+
+const getPokemonByName = async name => {
+  const { results: allPokemons } = await fetch(
+    "https://pokeapi.co/api/v2/pokemon/?limit=1000"
+  ).then(res => res.json());
+  return allPokemons.filter(pokemon => pokemon.name.includes(name));
+};
+
+let searchSubject$ = new BehaviorSubject("");
+let searchResultObservable$ = searchSubject$.pipe(
+  filter(val => val.length > 1),
+  debounceTime(750),
+  distinctUntilChanged(),
+  mergeMap(val => from(getPokemonByName(val)))
+);
+
+const useObservable$ = (observable, setter) => {
+  useEffect(() => {
+    let subscription = observable.subscribe(result => {
+      setter(result);
+    });
+    return () => subscription.unsubscribe();
+  }, [observable, setter]);
+};
 
 function App() {
+  const [search, setSearch] = useState("");
+  const [results, setResults] = useState([]);
+
+  useObservable$(searchResultObservable$, setResults);
+
+  const handleSearchChange = e => {
+    const newValue = e.target.value;
+    setSearch(newValue);
+    searchSubject$.next(newValue);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Pokemon app</h1>
+      <input
+        type="text"
+        placeholder="Search"
+        value={search}
+        onChange={handleSearchChange}
+      />
+      <div>
+        {results.map(pokemon => (
+          <div key={pokemon.name}>{pokemon.name}</div>
+        ))}
+      </div>
     </div>
   );
 }
